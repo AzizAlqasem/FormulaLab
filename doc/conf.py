021c133f7e297e9c0b8afc8fafcb6ac02470b3d5
AzizAlqasem <aziz_alqasem@hotmail.com>
2020-04-05 18:05:11 -0400

    New version 0.1.0
    This update is relativly stable and comprehensible (includs documentations). Therefore Version 0.0 ends here.

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Abdulaziz Alqasem'
 author = 'Abdulaziz Alqasem'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
