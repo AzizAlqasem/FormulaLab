6406d86ec72567bbfc41d57a05ee4e7fe012d044
AzizAlqasem <aziz_alqasem@hotmail.com>
2020-04-05 21:02:18 -0400

    Update webpage to v0.1.0

--- doc/_build/html/_static/bizstyle.js
+++ doc/_build/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("FormulaLab 0.0.4 documentation");
+        $("li.nav-item-0 a").text("FormulaLab 0.1.0 documentation");
     }
 });
